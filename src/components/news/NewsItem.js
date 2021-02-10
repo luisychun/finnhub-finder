6326a13bfecf6cbaf83b9774a296f4b3a6133c7d
Luis yChun <yongchun903@gmail.com>
2021-02-10 15:03:22 +0800

    style: refactor news badge styling

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import { Card, Button, Badge } from 'react-bootstrap'
 
-const NewsItem = ( {newsItem: {image, headline, summary, url, category}} ) => {
+const NewsItem = ({ newsItem }) => {
+  const { image, headline, summary, url, category, source } = newsItem
 
   return (
     <Card style={{ margin: '0.5rem' }}>
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{headline}</Card.Title>
-        <h5>
-          <Badge pill variant={category === 'top news'? 'dark': 'light'} style={{ marginBottom: '0.75rem' }}>
-            {category}
-          </Badge>
-        </h5>
-        <Card.Text>
-          {summary}
-        </Card.Text>
+        <Badge
+          pill
+          variant="info"
+          style={{ marginBottom: '0.75rem', marginRight: '0.5rem' }}
+        >
+          {source}
+        </Badge>
+        <Badge
+          pill
+          variant={category === 'top news' ? 'primary' : 'warning'}
+          style={{ marginBottom: '0.75rem' }}
+        >
+          {category}
+        </Badge>
+        <Card.Text>{summary}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-center">
-        <Button variant="dark" href={url} target="_blank" style={{ boxShadow: 'none' }}>Read More</Button>
+        <Button
+          variant="dark"
+          href={url}
+          target="_blank"
+          style={{ boxShadow: 'none' }}
+        >
+          Read More
+        </Button>
       </Card.Footer>
     </Card>
   )
